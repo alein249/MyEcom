f4130f93c766f2a214fbba6e5a011a61fe78ca77
Pankaj Sharma <panksh249@gmail.com>
2021-04-14 13:08:04 +0530

    Product

---
+++ src/models/Product.java
@@ -0,0 +1,20 @@
+package models;
+
+public class Product {
+    public String name;
+    public String imageUrl;
+    public Product(){
+    }
+    public Product(String name, String imageUrl){
+        this.name = name;
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", imageUrl = '" + imageUrl + '\'' +
+                '}';
+    }
+}
